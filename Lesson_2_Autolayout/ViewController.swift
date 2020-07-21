71e97d2bd66270c4b6df2731624790a8888ee3bd
Tanvi Prajapati <tanvi10072000@gmail.com>
2020-07-20 18:19:55 -0700

    first commit

---
+++ Lesson_2_Autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson_2_Autolayout
+//
+//  Created by Tanvi Prajapati on 17/07/20.
+//  Copyright © 2020 Tanvi Prajapati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
